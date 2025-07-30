ce12ce755c6a14a0927245332cfb23a7f74daf53
Tan Khay Liang <khayliang@macbook.local>
2025-07-30 22:50:33 +0800

    format component array for private member visibility

--- include/engine/ecs/component_array.hpp
+++ include/engine/ecs/component_array.hpp
@@ -14,69 +14,69 @@ template <typename T>
 class ComponentArray : public IComponentArray {
  public:
   const T& getData(Entity e) {
-    if (entityToIdxMapping.end() == entityToIdxMapping.find(e)) {
+    if (entityToIdxMapping_.end() == entityToIdxMapping_.find(e)) {
       throw std::out_of_range("Entity does not exist. Nothing to read");
     }
-    size_t idx = entityToIdxMapping[e];
-    return componentArray[idx];
+    size_t idx = entityToIdxMapping_[e];
+    return componentArray_[idx];
   }
 
   void insertData(Entity e, const T& data) {
-    if (entityToIdxMapping.find(e) != entityToIdxMapping.end()) {
+    if (entityToIdxMapping_.find(e) != entityToIdxMapping_.end()) {
       throw std::out_of_range("Entity already exists. Unable to insert");
     }
 
-    if (size == MAX_ENTITIES)
+    if (size_ == MAX_ENTITIES)
       throw std::out_of_range(
           "Max number of entities reached. Unable to insert entity");
-    entityToIdxMapping[e] = size;
-    idxToEntityMapping[size] = e;
-    componentArray[size] = data;
-    ++size;
+    entityToIdxMapping_[e] = size_;
+    idxToEntityMapping_[size_] = e;
+    componentArray_[size_] = data;
+    ++size_;
   }
 
   void updateData(Entity e, const T& data) {
-    if (entityToIdxMapping.end() == entityToIdxMapping.find(e)) {
+    if (entityToIdxMapping_.end() == entityToIdxMapping_.find(e)) {
       throw std::out_of_range("Entity does not exist. Unable to update");
     }
 
-    size_t idx = entityToIdxMapping[e];
-    componentArray[idx] = data;
+    size_t idx = entityToIdxMapping_[e];
+    componentArray_[idx] = data;
   }
 
   void upsertData(Entity e, const T& data) {
-    if (entityToIdxMapping.find(e) != entityToIdxMapping.end())
+    if (entityToIdxMapping_.find(e) != entityToIdxMapping_.end())
       updateData(e, data);
     else
       insertData(e, data);
   }
 
   void deleteEntity(Entity e) override {
-    if (entityToIdxMapping.end() == entityToIdxMapping.find(e)) {
+    if (entityToIdxMapping_.end() == entityToIdxMapping_.find(e)) {
       throw std::out_of_range("Entity does not exist. Unable to delete");
     }
-    size_t idxToRemove = entityToIdxMapping[e];
-    size_t idxLastElem = size - 1;
+    size_t idxToRemove = entityToIdxMapping_[e];
+    size_t idxLastElem = size_ - 1;
 
     if (idxToRemove == idxLastElem) {
-      entityToIdxMapping.erase(e);
-      idxToEntityMapping.erase(e);
-      --size;
+      entityToIdxMapping_.erase(e);
+      idxToEntityMapping_.erase(e);
+      --size_;
       return;
     }
 
-    Entity entityToMove = idxToEntityMapping[idxLastElem];
+    Entity entityToMove = idxToEntityMapping_[idxLastElem];
 
-    componentArray[idxToRemove] = std::move(componentArray[idxLastElem]);
-    entityToIdxMapping.erase(e);
-    entityToIdxMapping[entityToMove] = idxToRemove;
-    idxToEntityMapping[idxToRemove] = entityToMove;
-    --size;
+    componentArray_[idxToRemove] = std::move(componentArray_[idxLastElem]);
+    entityToIdxMapping_.erase(e);
+    entityToIdxMapping_[entityToMove] = idxToRemove;
+    idxToEntityMapping_[idxToRemove] = entityToMove;
+    --size_;
   };
 
  private:
-  size_t size = 0;
-  std::array<T, MAX_ENTITIES> componentArray{};
-  std::unordered_map<Entity, size_t> entityToIdxMapping{};
-  std::unordered_map<size_t, Entity> idxToEntityMapping{};
+  size_t size_ = 0;
+  std::array<T, MAX_ENTITIES> componentArray_{};
+  std::unordered_map<Entity, size_t> entityToIdxMapping_{};
+  std::unordered_map<size_t, Entity> idxToEntityMapping_{};
 };
